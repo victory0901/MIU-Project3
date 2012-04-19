839e5b3096c54cf300c07b722e35276298d8a04e
victory0901 <jfloyd@fullsail.edu>
2012-04-19 17:16:51 -0400

    Fixed minor issues

--- gold/main.js
+++ gold/main.js
@@ -19,9 +19,9 @@ $(document).ready(function(){
 			aierrorslink.click();
 			var html = '';
 			for(var key in validator.submitted){
-				var label = $('label[for^="'+ key +'"]');
+				var label = $('label[for^="'+ key +'"]').not('[generated]');
 				var legend = label.closest('fieldset').find('.ui-controlgroup-label');
-				var fieldName = legend.length ? legend.text() : legend.text();
+				var fieldName = legend.length ? legend.text() : label.text();
 				html += '<li>'+ fieldName +'</li>';
 			};
 			$("#additemerrors ul").html(html);
